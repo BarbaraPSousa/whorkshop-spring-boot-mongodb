4b2942b862c37d5f322a586a35f2c6e0d27c2440
Barbara Sousa <83707213+BarbaraPSousa@users.noreply.github.com>
2021-07-20 06:43:50 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/barbarasousa/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,48 @@
+//carregar dados das entidades de forma simples.
+package com.barbarasousa.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.barbarasousa.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+	}
+	
+	public UserDTO(User obj) { //contrutor p/ instanciar aparti do obj entity correspondente 
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/barbarasousa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/barbarasousa/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.barbarasousa.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barbarasousa.workshopmongo.domain.User;
+import com.barbarasousa.workshopmongo.dto.UserDTO;
 import com.barbarasousa.workshopmongo.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 	
 	//@GetMapping
 	@RequestMapping(method= RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {//Responsavel por retorna todos usuarios
+	public ResponseEntity<List<UserDTO>> findAll() {//Responsavel por retorna todos usuarios
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());// converção da list para listDTO
+		return ResponseEntity.ok().body(listDto);
 	}	
 }
